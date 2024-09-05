6b8d5cf0eeb18483de58e4033278e3cd9c93fdc2
Marlie Martinez <marlieahue@gmail.com>
2024-09-05 14:56:52 -0400

    created robin character and companion for her

--- app.js
+++ app.js
@@ -12,10 +12,16 @@ const adventurer = {
   },
 };
 class Character {
-    constructor(name) {
-      this.name = name;
-      this.health = 100;
-      this.inventory = [];
-    }
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
   }
-  
+}
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];

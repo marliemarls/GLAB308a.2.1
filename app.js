9f95b634173f6c1f570e5d1c4ccf65a9efc5cb28
Marlie Martinez <marlieahue@gmail.com>
2024-09-05 14:54:26 -0400

    character class created

---
+++ app.js
@@ -0,0 +1,21 @@
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+  },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
+};
+class Character {
+    constructor(name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+  }
+  
